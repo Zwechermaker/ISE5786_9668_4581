81bef5f425728a4dda3005806b659c7f5456b44d
dusprh <godfreysinboston@gmail.com>
2026-04-15 12:12:54 +0300

    render package added, declared methods for builder and camera

---
+++ src/renderer/Camera.java
@@ -0,0 +1,168 @@
+package renderer;
+
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.Locale;
+
+public class Camera implements  Cloneable {
+
+    /**
+     * The origin point of the camera.
+     */
+    private final Point _p0 = null;
+
+    /**
+     * The forward vector (towards the scene).
+     */
+    private final Vector _vTo = null;
+
+    /**
+     * The upward vector.
+     */
+    private final Vector _vUp = null;
+
+    /**
+     * The rightward vector.
+     */
+    private final Vector _vRight = null;
+
+    /**
+     * The width of the view plane.
+     */
+    private double _width;
+
+    /**
+     * The height of the view plane.
+     */
+    private double _height;
+
+    /**
+     * The distance from the camera to the view plane.
+     */
+    private double _distance;
+    /**
+     * center of the view plane.
+     */
+    private Point _vpCenter;
+    /**
+     * width of a pixel in the view plane.
+     */
+    private double _pixelWidth;
+    /**
+     * height of a pixel in the view plane.
+     */
+    private double _pixelHeight;
+
+    /**
+     * default constructor for camera
+     */
+    private Camera() {
+
+    }
+
+    /**
+     * ray constructor through a pixel
+     * @param column of the pixel
+     * @param row of the pixel
+     * @return the ray
+     */
+    public Ray constructRay(int column, int row){
+        return null;
+    }
+
+    /**
+     * a builder creator function for camera
+     * @return a new builder for camera
+     */
+    public static Builder getBuilder() {
+        return null;
+    }
+
+    /**
+     * a builder class for camera
+     */
+    public static class Builder {
+        /**
+         * camera object to build
+         */
+        private final Camera camera = new Camera();
+
+        /**
+         * setter for the origin point of the camera
+         * @param p0 the point to initialize
+         * @return a Builder for camera with the origin point
+         */
+        public Builder setLocation(Point p0) {
+            return null;
+        }
+
+        /**
+         * setter for the direction of the camera
+         * @param to vector pointing were the camera points to
+         * @param up vector pointing were the camer points up
+         * @return a Builder for camera with the set direction
+         */
+        public Builder setDirection(Vector to,Vector up) {
+            return null;
+        }
+
+        /**
+         * setter for the direction of the camera were the up vector is (0,1,0)
+         * @param target the point the camera points to
+         * @return a Builder for camera with the set direction
+         */
+        public Builder setDirection(Point target) {
+            return null;
+        }
+
+        /**
+         * setter for the direction of the camera
+         * @param target the point the camera points to
+         * @param up the vector the camera points up
+         * @return a Builder for camera with the set direction
+         */
+        public Builder setDirection(Point target, Vector up) {
+            return null;
+        }
+
+        /**
+         * setter for the resolution
+         * @param pixelWidth of a pixel
+         * @param pixelHeight of a pixel
+         * @return a Builder for camera with the resolution
+         */
+        public Builder setResolution(int pixelWidth, int pixelHeight) {
+            return null;
+        }
+
+        /**
+         * setter for the size of the view plane
+         * @param width of the view plane
+         * @param height of the view plane
+         * @return a Builder for camera with the size of the view plane
+         */
+        public Builder setVpSize(int width, int height){
+            return null;
+        }
+
+        /**
+         * setter for the distance from the camera to the view plane
+         * @param distance of the view plane
+         * @return a Builder for camera with the distance from the camera to the view plane
+         */
+        public Builder setVpDistance(double distance) {
+            return null;
+        }
+
+
+        /**
+         * builds the camera
+         * @return the camera
+         */
+        public Camera build() {
+            return null;
+        }
+    }
+}
