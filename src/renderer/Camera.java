45962bc44ffa549cd94cc42cb29a74b96b8acd0a
dusprh <godfreysinboston@gmail.com>
2026-06-07 12:48:35 +0300

    added multithreading found that the fastest was with 3 threads

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -87,7 +87,7 @@ public class Camera implements Cloneable {
 
     }
     /** Amount of threads to use fore rendering image by the camera */
-    private int threadsCount = 0;
+    private int threadsCount = 3;
     /**
      * Amount of threads to spare for Java VM threads:<br>
      * Spare threads if trying to use all the cores
